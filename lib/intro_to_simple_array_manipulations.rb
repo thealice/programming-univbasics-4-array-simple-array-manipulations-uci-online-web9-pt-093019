bc742cd3754fecd4ff64d7384e0c565491ef4775
Alice Freda <alicefreda@gmail.com>
2019-10-03 18:00:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,32 @@
 def using_push(array, element)
   array.push("#{element}")
 end
+
+def using_unshift(array, element)
+
+end
+
+def using_pop(array)
+
+end
+
+def pop_with_args(array)
+
+end
+
+def using_shift(array)
+
+end
+
+def shift_with_args(array)
+
+end
+
+def using_concat(array1, array2)
+
+end
+
+def using_insert(array, element)
+
+end
+end
